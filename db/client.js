795f6f66029f6c24e011ee525b2360bd02f42036
Elizabeth Lucky Paradis <lucky.paradis13@gmail.com>
2025-06-17 13:46:13 -0700

    Implement task

--- db/client.js
+++ db/client.js
@@ -1,11 +1,3 @@
 import pg from "pg";
-import dotenv from "dotenv";
-dotenv.config();
-
-const { Pool } = pg;
-
-const db = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
+const db = new pg.Client(process.env.DATABASE_URL);
 export default db;
