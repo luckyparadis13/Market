bae408cfb921d64915529b177170aea7ac533874
Elizabeth Lucky Paradis <lucky.paradis13@gmail.com>
2025-06-17 14:00:40 -0700

    Add protected route to view user’s orders

--- server.js
+++
@@ -1,8 +0,0 @@
-import app from "./app.js";
-import dotenv from "dotenv";
-dotenv.config();
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
-});
